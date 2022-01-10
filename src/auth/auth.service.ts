868df76bf123691e60010431a31bec4786ed26b7
Кузнецов Роман Игоревич <rokuznetsov@neoflex.ru>
2022-01-10 23:33:14 +0400

    token service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { SignOptions } from 'jsonwebtoken';
+import { TokenService } from 'src/token/token.service';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly jwtService: JwtService,
+        private readonly tokenService: TokenService,
+        private readonly userService: UserService
+    ) {}
+
+    signUp(createUserDto: CreateUserDto) {}
+
+    singIn(email: string, password: string) {}
+
+    async generateToken(data, signOptions?: SignOptions) {
+        return this.jwtService.sign(data, signOptions);
+    }
+
+    async verifyToken(token: string) {
+        try {
+            const data = this.jwtService.verify(token);
+            const tokenExist = this.tokenService.exists(data._id, token);
+            if(tokenExist) {
+                return data;
+            }
+            throw new UnauthorizedException();
+        } catch (error) {
+            throw new UnauthorizedException();
+        }
+    }
+}
